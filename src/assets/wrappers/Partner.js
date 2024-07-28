0076d192385859d8ee00189598094b28fc0ec591
ebubekeyz <ebubeofforjoe@gmail.com>
2024-07-28 22:22:31 +0100

    1

--- src/assets/wrappers/Partner.js
+++ src/assets/wrappers/Partner.js
@@ -6,7 +6,7 @@ const Wrapper = styled.div`
     padding: 2rem 1rem;
     display: flex;
     gap: 1rem;
-    background: rgba(235, 165, 16, 0.9);
+    background: rgba(235, 165, 16, 0.5);
     overflow-x: hidden;
     margin: 0 auto;
   }
